870b40a5528daf173256410739ddfee912e81e4e
Sandra <sandriniia_grebentcova@riseup.net>
2021-10-29 14:54:33 +0200

    change user model,default image, name and description

--- backend/models/user.js
+++ backend/models/user.js
@@ -9,17 +9,15 @@ const userSchema = new mongoose.Schema({
     type: String,
     minLength: 2,
     maxLength: 30,
-    default: 'Жак-Ив Кусто',
+    default: "Jeanne d'Arc",
   },
   about: {
     type: String,
-    minLength: 2,
-    maxLength: 30,
-    default: 'Исследователь',
+    default: '',
   },
   avatar: {
     type: String,
-    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    default: 'https://static-cdn.sr.se/images/2359/a4c28ebf-d089-42b1-afd3-ab8c5217d857.jpg',
     validate: {
       validator: (v) => validator.isURL(v, { disallow_auth: true, require_protocol: true }),
       message: 'Введите url',
@@ -41,19 +39,21 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email }).select('+password').then((user) => {
-    if (!user) {
-      return Promise.reject(new Unauthorized('Неправильные почта или пароль'));
-    }
-
-    return bcrypt.compare(password, user.password).then((matched) => {
-      if (!matched) {
+  return this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
         return Promise.reject(new Unauthorized('Неправильные почта или пароль'));
       }
 
-      return user;
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Unauthorized('Неправильные почта или пароль'));
+        }
+
+        return user;
+      });
     });
-  });
 };
 
 function toJSON() {
