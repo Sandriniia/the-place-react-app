613e1a2a30702c42430f056b28a909e837cc069c
Sandra <sandriniia_grebentcova@riseup.net>
2021-10-29 14:17:07 +0200

    translate to english InfoTooltip component

--- frontend/src/components/InfoTooltip.js
+++ frontend/src/components/InfoTooltip.js
@@ -5,18 +5,18 @@ import failImage from '../images/auth-fail.jpg';
 function InfoTooltip(props) {
   return (
     <div className={`popup ${props.isOpen ? '' : 'popup_hidden'}`}>
-      <div className="info-tooltip">
-        <button type="button" className="popup__close-button" onClick={props.onClose} />
-        <div className="info-tooltip__box">
+      <div className='info-tooltip'>
+        <button type='button' className='popup__close-button' onClick={props.onClose} />
+        <div className='info-tooltip__box'>
           <img
             src={props.isSuccess ? successImage : failImage}
-            alt=""
-            className="info-tooltip__image"
+            alt=''
+            className='info-tooltip__image'
           />
-          <p className="info-tooltip__text">
+          <p className='info-tooltip__text'>
             {props.isSuccess
-              ? 'Вы успешно зарегистрировались!'
-              : 'Что-то пошло не так!\n Попробуйте еще раз.'}
+              ? 'Your account has been created'
+              : 'Something went wrong.\n Please try again.'}
           </p>
         </div>
       </div>
