121da37fb7a17acd927cfba2d34be7e3f114c517
Sandra <sandriniia_grebentcova@riseup.net>
2021-10-29 14:27:43 +0200

    translate to english Header component; delte logo img, add h1 instead

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,41 +1,40 @@
 import React from 'react';
 import { Link, NavLink, Route, Switch } from 'react-router-dom';
-import logo from '../images/logo.svg';
 
 function Header({ isLogged, email, onLogout }) {
   return (
-    <header className="header">
-      <img src={logo} alt="Логотип:Место" className="header__logo" />
-      <nav className="header__nav">
-        <ul className="header__list">
+    <header className='header'>
+      <h1 className='header__logo'>The Place</h1>
+      <nav className='header__nav'>
+        <ul className='header__list'>
           <Switch>
-            <Route path="/sign-up">
-              <li className="header__nav-item">
+            <Route path='/sign-up'>
+              <li className='header__nav-item'>
                 <NavLink
-                  className="header__link"
-                  activeClassName="header__link_active"
-                  to="/sign-in"
+                  className='header__link'
+                  activeClassName='header__link_active'
+                  to='/sign-in'
                 >
-                  Войти
+                  Sign in
                 </NavLink>
               </li>
             </Route>
-            <Route path="/sign-in">
-              <li className="header__nav-item">
+            <Route path='/sign-in'>
+              <li className='header__nav-item'>
                 <NavLink
-                  className="header__link"
-                  activeClassName="header__link_active"
-                  to="/sign-up"
+                  className='header__link'
+                  activeClassName='header__link_active'
+                  to='/sign-up'
                 >
-                  Регистрация
+                  Sign up
                 </NavLink>
               </li>
             </Route>
             {isLogged ? (
               <>
-                <li className="header__nav-item header__email-item">{email}</li>
-                <Link to="" className="header__link" onClick={onLogout}>
-                  Выйти
+                <li className='header__nav-item header__email-item'>{email}</li>
+                <Link to='' className='header__link' onClick={onLogout}>
+                  Sign out
                 </Link>
               </>
             ) : (
